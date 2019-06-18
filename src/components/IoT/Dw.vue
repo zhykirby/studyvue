36ba420e132cf127c467392ada87e77cbe2be80e
zhykirby <zhykirby@qq.com>
2019-06-18 23:17:30 +0800

    Update Dw.vue

--- src/components/IoT/Dw.vue
+++ src/components/IoT/Dw.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
         <my-header :title="title"></my-header>
+        <div>
+            <ul>
+                <li v-for="(module,index) in modules" :key="index">
+                    <router-link :to="module.route" class="demo">
+                        <img src="../../assets/user.png" width="100px" height="100px">
+                        <div>
+                            <span class="title">这是一个标题</span>
+                            <div>
+                                <p>浏览次数：</p>
+                                <p>小标题：</p>
+                            </div>
+                        </div>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
         <h3>超声波定位的原理</h3>
         <p>
             超声波定位系统由若干个应答器和一个主测距器组成，主测距器放置在被测物体上，在微机指令信号的作用下向位置固定的应答器发射同频率的无线电信号，应答器在收
@@ -15,7 +31,10 @@ export default {
     name:'Dw',
     data(){
         return{
-            title:'无线定位系统'
+            title:'无线定位系统',
+            modules:[{
+                route:{name:'User'}
+            }]
         }
     },
     methods:{
@@ -27,5 +46,9 @@ export default {
 </script>
 
 <style scoped>
-
+ul{padding: 0;margin: 0;}
+li{list-style: none;height: 100px;}
+.demo{display: flex;flex-wrap: nowrap;}
+.title{height: 50px;line-height: 50px;width: 200px;font-size: 1.5rem;vertical-align: middle;margin: 0;}
+p{margin:0;}
 </style>
